d9b714b04bb667484efe9accdf507b7e8d76fd78
Hugo Flores Perez <hugofloresperez@Hugos-MacBook-Pro.local>
2020-06-11 16:43:13 -0400

    Initial Commit

---
+++ AppleRSSApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  AppleRSSApp
+//
+//  Created by Hugo Flores Perez on 6/11/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ AppleRSSApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AppleRSSApp
+//
+//  Created by Hugo Flores Perez on 6/11/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
